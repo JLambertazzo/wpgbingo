687eabe3f439eae536a6095328df9eac25c5aa57
Julien <jbertazzolambert@gmail.com>
2021-10-09 20:10:48 -0400

    added links and disclaimer

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 'use strict'
+const modal = picoModal({
+    content: '<p>I understand that cycling in Winnipeg includes areas where cyclists are not separated from traffic, that suggested routes may not reflect current conditions and that I am responsible for planning a route that suits current infrastructure and road conditions as well as my personal health and ability.</p> <button onclick="modal.close()">I accept</button>'
+}).show() // show disclaimer modal
 const images = document.querySelectorAll('img.card-img-top');
 const lightboxContainer = document.querySelector('.lightbox-shade')
 const cards = [...document.querySelectorAll('.card')].sort((a, b) => {
@@ -8,48 +11,60 @@ const randomMap = {
     0: {
         cards: [1, 2, 3, 4, 5],
         extras: [[6, 11], [10, 25]],
+        link: 'https://t.ly/pGrv',
     },
     1: {
         cards: [6, 7, 8, 9, 10],
         extras: [[11, 16], [20, 25]],
+        link: 'https://t.ly/Uqxh',
     },
     2: {
         cards: [11, 12, 13, 14, 15],
         extras: [[1, 6], [16, 21], [5, 10], [20, 25]],
+        link: 'https://t.ly/R2AH7',
     },
     3: {
         cards: [16, 17, 18, 19, 20],
         extras: [[6, 11], [10, 15]],
+        link: 'https://t.ly/OQl7',
     },
     4: {
         cards: [21, 22, 23, 24, 25],
         extras: [[11, 16], [15, 20]],
+        link: 'https://t.ly/3l1w',
     },
     5: {
         cards: [1, 6, 11, 16, 21],
         extras: [[2, 3], [22, 23]],
+        link: 'https://t.ly/5bsF',
     },
     6: {
         cards: [2, 7, 12, 17, 22],
         extras: [[3, 4], [23, 24]],
+        link: 'https://t.ly/YgcA',
     },
     7: {
         cards: [3, 8, 13, 18, 23],
         extras: [[1, 2], [4, 5], [21, 22], [24, 25]],
+        link: 'https://t.ly/PNBp',
     },
     8: {
         cards: [4, 9, 14, 19, 24],
         extras: [[2, 3], [22, 23]],
+        link: 'https://t.ly/4scx',
     },
     9: {
         cards: [5, 10, 15, 20, 25],
         extras: [[3, 4], [23, 24]],
+        link: 'https://t.ly/kPbz',
     },
     10: {
         cards: [1, 7, 13, 19, 25],
+        link: 'https://t.ly/n0uj',
     },
     11: {
-        cards: [5, 9, 13, 17, 21]
+        cards: [5, 9, 13, 17, 21],
+        link: 'https://t.ly/yRjz',
     },
 }
 let activeCards = null
@@ -78,7 +93,8 @@ function lightboxBlurHandler(target) {
 }
 
 function handleRandom() {
-    activeCards = randomMap[Math.floor(Math.random()*12)].cards
+    const selected = randomMap[Math.floor(Math.random()*12)]
+    activeCards = selected.cards
     console.log(activeCards)
     cards.forEach((card) => {
         if (activeCards.includes(+card.getAttribute('key'))) {
@@ -89,6 +105,10 @@ function handleRandom() {
             card.classList.add('inactive-card')
         }
     })
+    // show map it link
+    document.querySelector('.accept').classList.remove('hide');
+    const a = document.querySelector('#map-link')
+    a.setAttribute('href', selected.link)
 }
 
 function handleRandomSeven() {
@@ -106,6 +126,7 @@ function handleRandomSeven() {
             card.classList.add('inactive-card')
         }
     })
+    document.querySelector('.accept').classList.add('hide');
 }
 
 images.forEach(img => {
