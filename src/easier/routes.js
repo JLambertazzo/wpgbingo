c9d7819fa5d5c8b69092df9d66283d12a8de1878
Julien <jbertazzolambert@gmail.com>
2023-01-01 02:24:45 -0500

    feat: consolidate styles (old & new)

---
+++ src/easier/routes.js
@@ -0,0 +1,20 @@
+const routes = [
+    {ids: [1,2,3,4,5], url: 'https://t.ly/pGrv'},
+    {ids: [6,7,8,9,10], url: 'https://t.ly/fyJB'},
+    {ids: [11,12,13,14,15], url: 'https://t.ly/R2AH7'},
+    {ids: [16,17,18,19,20], url: 'https://t.ly/OQl7'},
+    {ids: [21,22,23,24,25], url: 'https://t.ly/3l1w'},
+    {ids: [1,6,11,16,21], url: 'https://t.ly/5bsF'},
+    {ids: [2,7,12,17,22], url: 'https://t.ly/YgcA'},
+    {ids: [3,8,13,18,23], url: 'https://t.ly/PNBp'},
+    {ids: [4,9,14,19,24], url: 'https://t.ly/4scx'},
+    {ids: [5,10,15,20,25], url: 'https://t.ly/a2uY'},
+    {ids: [1,7,13,19,25], url: 'https://t.ly/n0uj'},
+    {ids: [5,9,13,17,21], url: 'https://t.ly/yRjz'},
+    {ids: [1,2,3,6,8,11,12,13], url: 'https://t.ly/sHLy'},
+    {ids: [3,4,5,8,10,13,14,15], url: 'https://t.ly/erR8'},
+    {ids: [12,13,17,18,19,22,23,24], url: 'https://t.ly/LhWq'},
+    {ids: [11,12,13,16,18,21,22,23], url: 'https://t.ly/G4FH'}
+]
+
+const hardcodeRoute = {ids: new Array(25).fill(0).map((_, i) => i + 1), url: 'https://t.ly/BUCq'}
