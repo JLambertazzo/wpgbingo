b07f8bec3cd14a462ce903c58c9d0ed9c8df3047
Julien <jbertazzolambert@gmail.com>
2023-12-30 17:55:01 -0500

    feat: created history bingo page

---
+++ src/history/cards.js
@@ -0,0 +1,207 @@
+const cards = [
+  [
+    {
+      name: "Bittersweet Way",
+      description:
+        "Bittersweet Way, along Awasisak Mēskanow, details the history of this part of St. Vital.",
+      img: "/history/img/1-bittersweet.JPG",
+      full_img: "/history/img/1-bittersweet.JPG",
+    },
+    {
+      name: "La Barriere",
+      description:
+        "The La Barriere monument, on the Pembina highway, tells of Metis resistance.",
+      img: "/history/img/2-la-barriere.JPG",
+      full_img: "/history/img/2-la-barriere.JPG",
+    },
+    {
+      name: "St. Norbert Historical Park",
+      description: "A heritage park showing the history of St. Norbert.",
+      img: "/history/img/3-stn-hist-park.JPG",
+      full_img: "/history/img/3-stn-hist-park.JPG",
+    },
+    {
+      name: "Floodway",
+      description:
+        "A flood-prevention megaproject at Duff Roblin provincial park.",
+      img: "/history/img/4-floodway.JPG",
+      full_img: "/history/img/4-floodway.JPG",
+    },
+    {
+      name: "Monastery",
+      description: "The ruins of a monastery along the La Salle river.",
+      img: "/history/img/5-monastery.JPG",
+      full_img: "/history/img/5-monastery.JPG",
+    },
+  ],
+  [
+    {
+      name: "Louis Riel Sr. Mill",
+      description:
+        "Louis Riel’s father operated a mill here on the Seine river.",
+      img: "/history/img/6-riel-mill.JPG",
+      full_img: "/history/img/6-riel-mill.JPG",
+    },
+    {
+      name: "Legislature",
+      description: "Statues and monuments adorn the legislature grounds.",
+      img: "/history/img/7-legislature.JPG",
+      full_img: "/history/img/7-legislature.JPG",
+    },
+    {
+      name: "The Forks",
+      description: "The Forks is where it all began for what is now Winnipeg.",
+      img: "/history/img/8-forks.JPG",
+      full_img: "/history/img/8-forks.JPG",
+    },
+    {
+      name: "Grant's Old Mill",
+      description: "A recreation of a mill operated by Cuthbert Grant.",
+      img: "/history/img/9-grants.JPG",
+      full_img: "/history/img/9-grants.JPG",
+    },
+    {
+      name: "Stopping the Survey",
+      description: "Riel and his men confronted uninvited surveyors.",
+      img: "/history/img/10-survey.JPG",
+      full_img: "/history/img/10-survey.JPG",
+    },
+  ],
+  [
+    {
+      name: "The Mint",
+      description:
+        "Countries around the world, shown by flags, use coins produced here.",
+      img: "/history/img/11-mint.JPG",
+      full_img: "/history/img/11-mint.JPG",
+    },
+    {
+      name: "Upper Fort Garry",
+      description: "The fort where the path to provincehood was laid.",
+      img: "/history/img/12-ufg.JPG",
+      full_img: "/history/img/12-ufg.JPG",
+    },
+    {
+      name: "Early Skyscrapers",
+      description: "A tribute to boom times and historic architecture.",
+      img: "/history/img/13-skyscrapers.JPG",
+      full_img: "/history/img/13-skyscrapers.JPG",
+    },
+    {
+      name: "Air Force Way",
+      description: "Historic military planes lie along Air Force Way.",
+      img: "/history/img/14-air-force.jpg",
+      full_img: "/history/img/14-air-force.jpg",
+    },
+    {
+      name: "Pacific Junction",
+      description: "A small train station lay here.",
+      img: "/history/img/15-pacific-junction.JPG",
+      full_img: "/history/img/15-pacific-junction.JPG",
+    },
+  ],
+  [
+    {
+      name: "Shoal Lake Aqueduct",
+      description:
+        "A tribute to the faraway source of Winnipeg’s drinking water.",
+      img: "/history/img/16-shoal.JPG",
+      full_img: "/history/img/16-shoal.JPG",
+    },
+    {
+      name: "St. Boniface Cathedral",
+      description: "The cathedral in the heart of St. Boniface.",
+      img: "/history/img/17-cathedral.jpg",
+      full_img: "/history/img/17-cathedral.jpg",
+    },
+    {
+      name: "Bloody Saturday",
+      description: "A monument to the general strike of 1919.",
+      img: "/history/img/18-bloody-sat.JPG",
+      full_img: "/history/img/18-bloody-sat.JPG",
+    },
+    {
+      name: "McClung Park",
+      description: "A small park named after a fighter for equality.",
+      img: "/history/img/19-mcclung.JPG",
+      full_img: "/history/img/19-mcclung.JPG",
+    },
+    {
+      name: "Citizen's Hall of Fame",
+      description: "Busts of famous Winnipeg residents sit along the path.",
+      img: "/history/img/20-chof.JPG",
+      full_img: "/history/img/20-chof.JPG",
+    },
+  ],
+  [
+    {
+      name: "CNR 2747",
+      description: "The first steam locomotive built in western Canada.",
+      img: "/history/img/21-cn2747.JPG",
+      full_img: "/history/img/21-cn2747.JPG",
+    },
+    {
+      name: "Elzear Goulet Park",
+      description:
+        "A memorial to a Metis leader killed while trying to escape across the river.",
+      img: "/history/img/22-elzear.JPG",
+      full_img: "/history/img/22-elzear.JPG",
+    },
+    {
+      name: "Ross House",
+      description: "The first post office in western Canada.",
+      img: "/history/img/23-ross-house.JPG",
+      full_img: "/history/img/23-ross-house.JPG",
+    },
+    {
+      name: "Ukrainian Labour Temple",
+      description:
+        "A hub for Ukrainian social and cultural activities, and for the labour movement.",
+      img: "/history/img/24-ukr-temple.JPG",
+      full_img: "/history/img/24-ukr-temple.JPG",
+    },
+    {
+      name: "Assiniboia School",
+      description:
+        "The site of a former residential school, with a monument to the children.",
+      img: "/history/img/25-res-school.JPG",
+      full_img: "/history/img/25-res-school.JPG",
+    },
+  ],
+  [
+    {
+      name: "Transcona Cordite Plant",
+      description:
+        "A former munitions plant and, briefly, a Japanese internment camp.",
+      img: "/history/img/26-tcona-plant.JPG",
+      full_img: "/history/img/26-tcona-plant.JPG",
+    },
+    {
+      name: "Paddle to the Amazon",
+      description:
+        "The Starkells paddled a dangerous 12,000 miles to Brazil, starting here.",
+      img: "/history/img/27-amazon-paddle.JPG",
+      full_img: "/history/img/27-amazon-paddle.JPG",
+    },
+    {
+      name: "Chief Peguis/Tommy Price",
+      description:
+        "Statues of two great First Nation men, Chief Peguis and Tommy Prince.",
+      img: "/history/img/28-peguis-price.JPG",
+      full_img: "/history/img/28-peguis-price.JPG",
+    },
+    {
+      name: "Battle of Seven Oaks",
+      description: "Site of a bloody fur-trade era battle.",
+      img: "/history/img/29-battle-7oaks.JPG",
+      full_img: "/history/img/29-battle-7oaks.JPG",
+    },
+    {
+      name: "Valour Road Plaza",
+      description:
+        "Three men, from this one street, were awarded the Victoria Cross.",
+      img: "/history/img/30-valour-rd.JPG",
+      full_img: "/history/img/30-valour-rd.JPG",
+    },
+  ],
+];
---
+++ src/history/routes.js
@@ -0,0 +1,20 @@
+const routes = [
+    {ids: [1,2,3,4,5], url: 'https://bit.ly/history-r1'},
+    {ids: [6,7,8,9,10], url: 'https://bit.ly/history-r2'},
+    {ids: [11,12,13,14,15], url: 'https://bit.ly/history-r3'},
+    {ids: [16,17,18,19,20], url: 'https://bit.ly/history-r4'},
+    {ids: [21,22,23,24,25], url: 'https://bit.ly/history-r5'},
+    {ids: [26,27,28,29,30], url: 'https://bit.ly/history-r6'},
+    {ids: [1,6,11,16,21], url: 'https://bit.ly/history-c1'},
+    {ids: [2,7,12,17,22], url: 'https://bit.ly/history-c2'},
+    {ids: [3,8,13,18,23], url: 'https://bit.ly/history-c3'},
+    {ids: [4,9,14,19,24], url: 'https://bit.ly/history-c4'},
+    {ids: [5,10,15,20,25], url: 'https://bit.ly/history-c5'},
+    {ids: [7,12,8,18,13], url: 'https://bit.ly/history-central-loop'},
+    {ids: [8,16,21,26,28,27], url: 'https://bit.ly/history-east-loop'},
+    {ids: [1,6,11,17,22,8], url: 'https://bit.ly/history-southeast-loop'},
+    {ids: [14,9,15,25], url: 'https://bit.ly/history-west-loop'},
+    {ids: [28,29,23,8,16,27], url: 'https://bit.ly/history-north-loop'}
+]
+
+const hardcodeRoute = {ids: new Array(30).fill(0).map((_, i) => i + 1), url: 'https://bit.ly/history-hardcore'}
