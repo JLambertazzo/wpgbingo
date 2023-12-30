7e14d6540ee4599b2b684cf7ebaf84ed3de84fb1
Julien <jbertazzolambert@gmail.com>
2023-12-30 18:21:27 -0500

    fix: wrong site on west loop

--- src/history/routes.js
+++ src/history/routes.js
@@ -13,7 +13,7 @@ const routes = [
     {ids: [7,12,8,18,13], url: 'https://bit.ly/history-central-loop'},
     {ids: [8,16,21,26,27], url: 'https://bit.ly/history-east-loop'},
     {ids: [1,6,11,17,22,8], url: 'https://bit.ly/history-southeast-loop'},
-    {ids: [14,9,15,25], url: 'https://bit.ly/history-west-loop'},
+    {ids: [14,9,15,20], url: 'https://bit.ly/history-west-loop'},
     {ids: [28,29,23,8,16,27], url: 'https://bit.ly/history-north-loop'}
 ]
 
