92e3ff1f2f14f00d06637ed0f9d3e0ea1bd15237
Colin Viebrock <colin@viebrock.ca>
2024-02-26 10:15:07 -0600

    fix ligatures, style 404

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -9,8 +9,24 @@ useHead({
 
 <template>
   <main>
-    <h1>Not Found</h1>
+    <h1><span>404</span> Not Found</h1>
   </main>
 </template>
 
-<style></style>
+<style scoped>
+main {
+  height: calc(100vh - 4rem);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+h1 {
+  font-size: 2rem;
+  span {
+    font-weight: var(--weight-thin);
+    padding-right: 0.5rem;
+    border-right: 1px solid var(--gray-light);
+    margin-right: 0.5rem;
+  }
+}
+</style>
