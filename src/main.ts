e9e11b607d9bfb90c0995b5d5bedee6a0ab9f944
Colin Viebrock <colin@viebrock.ca>
2024-03-02 11:43:31 -0600

    add initial GTM support

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,19 @@ import './assets/main.scss';
 
 import { createApp } from 'vue';
 import { createHead } from '@unhead/vue';
+import { createGtm } from '@gtm-support/vue-gtm';
 
 import App from './App.vue';
 import router from './router';
 
 const app = createApp(App);
-app.use(createHead());
 app.use(router);
+app.use(createHead());
+app.use(
+  createGtm({
+    id: 'GTM-577DCV5D',
+    vueRouter: router,
+  }),
+);
 
 app.mount('#app');
