eba02991f373c2e8eded957fde9d2159f4928bad
Julien Bertazzo Lambert <jbertazzolambert@gmail.com>
2025-01-11 11:37:05 -0500

    feat: create bingo template & migrate easier bingo

--- src/js/easier/cards.js
+++ src/js/easier/cards.js
@@ -3,160 +3,160 @@ const cards = [
         {
             name: 'Seine River @ Perimeter',
             description: 'An old prop from the Manitoba Museum, the hand is on private property across the Seine river from a public path, just inside the Perimeter highway.',
-            img: '/easier/img/1seineriveratperimeter.jpeg',
-            full_img: '/easier/img/1seineriveratperimeter.jpeg'
+            img: '/img/easier/1seineriveratperimeter.jpeg',
+            full_img: '/img/easier/1seineriveratperimeter.jpeg'
         },
         {
             name: 'Awasisak Mēskanow',
             description: 'The antlers are right beside the greenway, a little west of Ste Anne’s Rd.',
-            img: '/easier/img/2awasisakmeskanow.jpeg',
-            full_img: '/easier/img/2awasisakmeskanow.jpeg'
+            img: '/img/easier/2awasisakmeskanow.jpeg',
+            full_img: '/img/easier/2awasisakmeskanow.jpeg'
         },
         {
             name: 'Princess Auto Stadium',
             description: 'The home of the Winnipeg Blue Bombers on the University of Manitoba campus.',
-            img: '/easier/img/3princessautostadium.jpeg',
-            full_img: '/easier/img/3princessautostadium.jpeg'
+            img: '/img/easier/3princessautostadium.jpeg',
+            full_img: '/img/easier/3princessautostadium.jpeg'
         },
         {
             name: 'Fort Whyte Alive',
             description: 'The tipi is on the main walking/cycling path through Fort Whyte Alive.',
-            img: '/easier/img/4fortwhytealive.jpeg',
-            full_img: '/easier/img/4fortwhytealive.jpeg'
+            img: '/img/easier/4fortwhytealive.jpeg',
+            full_img: '/img/easier/4fortwhytealive.jpeg'
         },
         {
             name: 'The Leaf',
             description: 'The Leaf is the newest attraction at Assiniboine Park.',
-            img: '/easier/img/5theleaf.jpeg',
-            full_img: '/easier/img/5theleaf.jpeg'
+            img: '/img/easier/5theleaf.jpeg',
+            full_img: '/img/easier/5theleaf.jpeg'
         }
     ],
     [
         {
             name: 'Bois des Esprit',
             description: 'This carving, just south of John Bruce Rd, is one of more than 20 that can be found along the trails in Bois Des Esprits.',
-            img: '/easier/img/6boisdesesprit.jpeg',
-            full_img: '/easier/img/6boisdesesprit.jpeg'
+            img: '/img/easier/6boisdesesprit.jpeg',
+            full_img: '/img/easier/6boisdesesprit.jpeg'
         },
         {
             name: 'Elm Park Bridge',
             description: 'The Elm Park Bridge connects Kingston Crescent and Jubilee, next to a well-known ice cream venue.',
-            img: '/easier/img/7elmparkbridge.jpeg',
-            full_img: '/easier/img/7elmparkbridge.jpeg'
+            img: '/img/easier/7elmparkbridge.jpeg',
+            full_img: '/img/easier/7elmparkbridge.jpeg'
         },
         {
             name: 'Rooster Town Kettle',
             description: 'The kettle, at Beaumont station, is one of several pieces of public art along the Southwest transitway path.',
-            img: '/easier/img/8roostertownkettle.jpeg',
-            full_img: '/easier/img/8roostertownkettle.jpeg'
+            img: '/img/easier/8roostertownkettle.jpeg',
+            full_img: '/img/easier/8roostertownkettle.jpeg'
         },
         {
             name: 'Lyric Theater',
             description: 'The Lyric Theatre is a popular venue in Assiniboine Park.',
-            img: '/easier/img/9lyrictheater.jpeg',
-            full_img: '/easier/img/9lyrictheater.jpeg'
+            img: '/img/easier/9lyrictheater.jpeg',
+            full_img: '/img/easier/9lyrictheater.jpeg'
         },
         {
             name: 'Grant\'s Old Mill',
             description: 'A recreation of a grain mill that operated circa 1830. It lies along the Sturgeon Creek Greenway trail.',
-            img: '/easier/img/10grantsoldmill.JPG',
-            full_img: '/easier/img/10grantsoldmill.JPG'
+            img: '/img/easier/10grantsoldmill.JPG',
+            full_img: '/img/easier/10grantsoldmill.JPG'
         }
     ],
     [
         {
             name: 'St. Boniface Cathedral',
             description: 'The Saint Boniface Cathedral is across the Red River from The Forks.',
-            img: '/easier/img/11stbcathedral.jpeg',
-            full_img: '/easier/img/11stbcathedral.jpeg'
+            img: '/img/easier/11stbcathedral.jpeg',
+            full_img: '/img/easier/11stbcathedral.jpeg'
         },
         {
             name: 'Canadian Museum for Human Rights',
             description: 'The Canadian Museum For Human Rights is the tallest building at The Forks.',
-            img: '/easier/img/12cmhr.jpeg',
-            full_img: '/easier/img/12cmhr.jpeg'
+            img: '/img/easier/12cmhr.jpeg',
+            full_img: '/img/easier/12cmhr.jpeg'
         },
         {
             name: 'The Forks',
             description: 'The Forks is a popular destination for summer patio fun and winter skating.',
-            img: '/easier/img/13theforks.jpeg',
-            full_img: '/easier/img/13theforks.jpeg'
+            img: '/img/easier/13theforks.jpeg',
+            full_img: '/img/easier/13theforks.jpeg'
         },
         {
             name: 'Assiniboine Park',
             description: 'The pavillion at Assiniboine Park is the park’s focal point and sits near the centre.',
-            img: '/easier/img/14assiniboinepark.jpeg',
-            full_img: '/easier/img/14assiniboinepark.jpeg'
+            img: '/img/easier/14assiniboinepark.jpeg',
+            full_img: '/img/easier/14assiniboinepark.jpeg'
         },
         {
             name: 'Air Force Way',
             description: 'A collection of old planes lies alongside Air Force Way, accessible via the Yellow Ribbon Greenway trail.',
-            img: '/easier/img/15airforceway.jpeg',
-            full_img: '/easier/img/15airforceway.jpeg'
+            img: '/img/easier/15airforceway.jpeg',
+            full_img: '/img/easier/15airforceway.jpeg'
         }
     ],
     [
         {
             name: 'Transcona Bioreserve',
             description: 'The Transcona Bioreserve is a lovely greenspace off the Transcona Trail.',
-            img: '/easier/img/16transconabioreserve.jpeg',
-            full_img: '/easier/img/16transconabioreserve.jpeg'
+            img: '/img/easier/16transconabioreserve.jpeg',
+            full_img: '/img/easier/16transconabioreserve.jpeg'
         },
         {
             name: 'Fort Gibraltar',
             description: 'Fort Gibraltar is a replica of a fur trading post in Whittier Park, close to a trail that links the Forks with the mouth of the Seine River.',
-            img: '/easier/img/17fortgibraltar.jpeg',
-            full_img: '/easier/img/17fortgibraltar.jpeg'
+            img: '/img/easier/17fortgibraltar.jpeg',
+            full_img: '/img/easier/17fortgibraltar.jpeg'
         },
         {
             name: 'Blue Cross Park',
             description: 'Blue Cross Park is the home of the Winnipeg Goldeyes and is near the start of the North Winnipeg Parkway trail.',
-            img: '/easier/img/18bluecrosspark.jpeg',
-            full_img: '/easier/img/18bluecrosspark.jpeg'
+            img: '/img/easier/18bluecrosspark.jpeg',
+            full_img: '/img/easier/18bluecrosspark.jpeg'
         },
         {
             name: 'Manitoba Legislature',
             description: 'The Manitoba legislature is the grandest building in the city, and sits north of the bike trail that connects The Forks to West Broadway.',
-            img: '/easier/img/19mbleg.jpeg',
-            full_img: '/easier/img/19mbleg.jpeg'
+            img: '/img/easier/19mbleg.jpeg',
+            full_img: '/img/easier/19mbleg.jpeg'
         },
         {
             name: 'Brookside Cemetery',
             description: 'Brookside Cemetery is a resting place for many war veterans, politicians and other renowned Winnipeggers. Next door is Red  River College, the end point of a few bike paths.',
-            img: '/easier/img/20brooksidecemetery.jpeg',
-            full_img: '/easier/img/20brooksidecemetery.jpeg'
+            img: '/img/easier/20brooksidecemetery.jpeg',
+            full_img: '/img/easier/20brooksidecemetery.jpeg'
         }
     ],
     [
         {
             name: 'End of Transcona Trail',
             description: 'The Transcona trail ends in a small loop next to the Perimeter highway.',
-            img: '/easier/img/21endoftransconatrail.jpeg',
-            full_img: '/easier/img/21endoftransconatrail.jpeg'
+            img: '/img/easier/21endoftransconatrail.jpeg',
+            full_img: '/img/easier/21endoftransconatrail.jpeg'
         },
         {
             name: 'Bunn\'s Creek',
             description: 'The Bunn’s Creek trail runs from the Northeast Pioneers Greenway to the Red  river.',
-            img: '/easier/img/22bunnscreek.jpeg',
-            full_img: '/easier/img/22bunnscreek.jpeg'
+            img: '/img/easier/22bunnscreek.jpeg',
+            full_img: '/img/easier/22bunnscreek.jpeg'
         },
         {
             name: 'Chief Peguis Trail',
             description: 'The wolves lie along Chief Peguis Trail a little east of the Red river.',
-            img: '/easier/img/23chiefpeguistrail.jpeg',
-            full_img: '/easier/img/23chiefpeguistrail.jpeg'
+            img: '/img/easier/23chiefpeguistrail.jpeg',
+            full_img: '/img/easier/23chiefpeguistrail.jpeg'
         },
         {
             name: 'Kildonan Park',
             description: 'Rainbow Stage theatre sits near the Red River in Kildonan Park, close to the North Winnipeg Parkway trail.',
-            img: '/easier/img/24kildonanpark.jpeg',
-            full_img: '/easier/img/24kildonanpark.jpeg'
+            img: '/img/easier/24kildonanpark.jpeg',
+            full_img: '/img/easier/24kildonanpark.jpeg'
         },
         {
             name: 'Pilehenge',
             description: 'Winnipeg\'s answer to Stonehenge. Concrete piles stacked in circles - the remnants of an old cement factory. it\'s just outside city limits on Sturgeon Rd in the RM of Rosser. It\'s a few kilometres from any protected cycling infrastructure. Proceed with caution.',
-            img: '/easier/img/25pilehenge.jpeg',
-            full_img: '/easier/img/25pilehenge.jpeg'
+            img: '/img/easier/25pilehenge.jpeg',
+            full_img: '/img/easier/25pilehenge.jpeg'
         }
     ],
 ]
